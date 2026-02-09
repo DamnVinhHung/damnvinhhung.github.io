42031551a2451f8e55074969980901daa86c9973
Smith au Lait <git@smith.aulait.org>
2026-02-09 06:06:13 +0000

    mod: enable single line break

--- quartz.config.ts
+++ quartz.config.ts
@@ -73,6 +73,7 @@ const config: QuartzConfig = {
       }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "mathjax" }),
+      Plugin.HardLineBreaks(),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
